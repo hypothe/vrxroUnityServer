0e5ab4d572ae856ce690e5cafffa6b0fefb624b6
hypothe <markeagle98@gmail.com>
2021-11-17 21:50:29 +0000

    <Add> current status set and retrieval

--- app.js
+++ app.js
@@ -8,36 +8,51 @@ const io = new Server(server);
 
 const port = 4242;
 
-const EControlState = { NONE: 0, ROT_L: 1, ROT_R: 2, ROT_U: 3, ROT_D: 4, COLOR: 5 };
+const EControlState = { NONE: 'NONE', ROT_L: 'ROT_L', ROT_R: 'ROT_R', ROT_U: 'ROT_U', ROT_D: 'ROT_D', COLOR: 'COLOR' };
 
 var currentState = EControlState.NONE;
 var btnCount = 0;
 
-app.get("/unity", (req, res) => {
-	// Unity side
+// Unity side
 
-	// the app continuously polls the server to retrieve 
-	// the current state (rotate L/R/U/D, change color)
+// the app continuously polls the server to retrieve 
+// the current state (rotate L/R/U/D, change color)
+app.get("/unity", (req, res) => {
+	console.log("Unity request.")
+	res.send(currentState);
 });
 
-app.get("/user", (req, res) => {
-	// User side
+// User side
 
-	// the user will see an HTML page with 5 buttons
-	// pressing them will change the state
+// the user will see an HTML page with 5 buttons
+// pressing them will change the state
+app.get("/user", (req, res) => {
 	res.sendFile(__dirname + '/static/user.html')
 });
 
+// The socket receives an message each time an event is
+// triggered in the html page passed to the user, in the
+// case when a button is clicked.
+// The 'name' of the last button pressed is saved as the
+// current status
 io.on("connection", (socket) => {
 	console.log("User connected");
 
 	socket.on("disconnect", () => {
 		console.log("User disconnected")
 	});
-	// TODO: parse and save into internal status
+
 	socket.on("btnClick", (msg) => {
-		console.log(msg);
 		btnCount++;
+		
+		if (msg.name in EControlState) {
+			currentState = EControlState[msg.name];
+		}
+		else {
+			currentState = EControlState.NONE;
+		}
+		console.log(currentState);
+
 		io.emit('btnReply', btnCount);
 	})
 });
