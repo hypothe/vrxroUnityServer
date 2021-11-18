7e97e8c833641a940066a624b6759e4f92b8385e
hypothe <markeagle98@gmail.com>
2021-11-18 12:05:08 +0000

    <Add> gyro control stub

--- app_gyro.js
+++ app_gyro.js
@@ -7,10 +7,12 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const port = 4242;
-
+// TODO: clean, only color remains
 const EControlState = { NONE: 'NONE', ROT_L: 'ROT_L', ROT_R: 'ROT_R', ROT_U: 'ROT_U', ROT_D: 'ROT_D', COLOR: 'COLOR', RESET: 'RESET'  };
 
-const currentState = { state: EControlState.NONE, id: 0 };
+
+const currentCommand = {x: 0.0, y: 0.0, z: 0.0};
+const currentState = { state: EControlState.NONE, id: 0, orientation: currentCommand };
 //var btnCount = 0;
 
 // Unity side
@@ -27,7 +29,7 @@ app.get("/unity", (req, res) => {
 // the user will see an HTML page with 5 buttons
 // pressing them will change the state
 app.get("/user", (req, res) => {
-	res.sendFile(__dirname + '/static/user.html')
+	res.sendFile(__dirname + '/static/user_gyro.html')
 });
 
 // The socket receives an message each time an event is
@@ -54,6 +56,11 @@ io.on("connection", (socket) => {
 		console.log(currentState.state);
 
 		io.emit('btnReply', currentState.id);
+	})
+
+	socket.on("deviceOrientation", (msg) => {
+		// take the components and update
+		// currentState.orientation
 	})
 });
 
